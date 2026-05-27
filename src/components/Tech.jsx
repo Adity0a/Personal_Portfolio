8987164ca634e997200d42eff061912777d90599
Aaditya <a67i775525@gmail.com>
2026-05-27 13:03:19 +0530

    done!!

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
@@ -6,6 +7,29 @@ import { textVariant } from "../utils/motion";
 import { styles } from "../styles";
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -14,8 +38,18 @@ const Tech = () => {
       </motion.div>
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+          <div className="w-28 h-28 flex justify-center items-center" key={technology.name}>
+            {isMobile ? (
+              <div className="w-full h-full flex justify-center items-center bg-tertiary rounded-full p-4 border-2 border-white/10 shadow-card hover:border-[#915EFF]/50 transition-all duration-300 backdrop-blur-sm">
+                <img
+                  src={technology.icon}
+                  alt={technology.name}
+                  className="w-16 h-16 object-contain"
+                />
+              </div>
+            ) : (
+              <BallCanvas icon={technology.icon} />
+            )}
           </div>
         ))}
       </div>
